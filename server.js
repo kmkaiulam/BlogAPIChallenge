b56988e8244ad4c52b51b30c9ff29ae777e67d01
Kenny Lam <kmlam@ucdavis.edu>
2018-05-17 14:07:12 -0700

    add POST and PUT

--- server.js
+++ server.js
@@ -7,13 +7,58 @@ const {BlogPosts} = require('./models');
 const jsonParser = bodyParser.json();
 const app = express();
 
+
+//GET
 app.get('/blogChallenge', (req, res) => {
     res.json(BlogPosts.get());
 });
 
+//POST
+app.post('/blogChallenge', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++){
+        const field =requiredFields[i];
+        if(!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    const entry = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.status(201).json(entry);
+});
+
+//PUT
+app.put('/blogChallenge', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author', 'id'];
+    for (let i=0; i<requiredFields.length; i++){
+        const field =requiredFields[i];
+        if(!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id){
+        const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    console.log(`Updating blog post entry \`${req.params.id}\``);
+    BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author,
+        publishDate: req.body.publishDate,
+    });
 
 
+    }
+}
+);
 
+//DELETE
 
 
 
