5d400e684c23326543be518b18f6b1d7062f1eff
Kenny Lam <kmlam@ucdavis.edu>
2018-05-22 13:58:56 -0700

    added GET tests

--- tests/test-blog.js
+++ tests/test-blog.js
@@ -22,6 +22,23 @@ describe('Blog Posts', function() {
     return closeServer();
   });
 
-  it()
-
-});
+  it('should list Blog posts on GET', function(){
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res){ 
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(1);
+        const expectedKeys= ['id', 'title', 'content', 'author'];
+        res.body.forEach(function(blogpost){
+          expect(blogpost).to.be.a('object');
+          expect(blogpost).to.include.keys(expectedKeys);
+          expect(blogpost.id).to.not.equal(null || undefined);
+          expect(blogpost.title).to.be.a('string');
+          expect(blogpost.content).to.be.a('string');
+          expect(blopost.author).to.be.a('string');
+        });
+      });
+    });  
+  });
