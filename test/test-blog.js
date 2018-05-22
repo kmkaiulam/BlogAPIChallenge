d2d5d0d668a8dba61710f3f2dad9e5885ec51c67
Kenny Lam <kmlam@ucdavis.edu>
2018-05-22 15:22:29 -0700

    fixed POST test endpoint issue

--- test/test-blog.js
+++ test/test-blog.js
@@ -41,35 +41,27 @@ describe('Blog Posts', function() {
           expect(blogpost.publishDate).to.be.a('number');
         });
       });
-    });  
+    });
+      
     
     //create a blog post to POST and send
 
-    it('should list create a new blog post on POST', function(){
+    it('should create a new blog post on POST', function(){
       const newPost = {title: 'newTestPost', content: 'testing', author: 'Kenny'}
       return chai.request(app)
-      .post('blog-posts')
+      .post('/blog-posts')
       .send(newPost)
       .then(function(res){
-        expect(res).to.have.status(201)
-        expect(res).to.be.a.json
-        expect(res.body).to.be.a('array')
-        const expectedKeys= ['id', 'title', 'content', 'author', 'publishDate'];
-        res.body.forEach(function(blogpost){
-          expect(blogpost).to.be.a('object');
-          expect(blogpost).to.include.keys(expectedKeys);
-          expect(blogpost.id).to.not.equal(null || undefined);
-          expect(blogpost.title).to.be.a('string');
-          expect(blogpost.content).to.be.a('string');
-          expect(blogpost.author).to.be.a('string');
-          expect(blogpost.publishDate).to.be.a('number');
+        expect(res).to.have.status(201);
+        expect(res).to.be.a.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
+        expect(res.body).to.deep.equal(Object.assign(newPost, {id: res.body.id, publishDate: res.body.publishDate}));
         });
       });
     });
-  
-  
-  
-  
-  
-  
-  });
+
+    
+
+
+
