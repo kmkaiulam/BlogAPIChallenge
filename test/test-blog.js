b1da6f3eecfe2cde4720181c327c7b9812251265
Kenny Lam <kmlam@ucdavis.edu>
2018-05-22 15:05:03 -0700

    added post test

--- test/test-blog.js
+++ test/test-blog.js
@@ -42,4 +42,34 @@ describe('Blog Posts', function() {
         });
       });
     });  
+    
+    //create a blog post to POST and send
+
+    it('should list create a new blog post on POST', function(){
+      const newPost = {title: 'newTestPost', content: 'testing', author: 'Kenny'}
+      return chai.request(app)
+      .post('blog-posts')
+      .send(newPost)
+      .then(function(res){
+        expect(res).to.have.status(201)
+        expect(res).to.be.a.json
+        expect(res.body).to.be.a('array')
+        const expectedKeys= ['id', 'title', 'content', 'author', 'publishDate'];
+        res.body.forEach(function(blogpost){
+          expect(blogpost).to.be.a('object');
+          expect(blogpost).to.include.keys(expectedKeys);
+          expect(blogpost.id).to.not.equal(null || undefined);
+          expect(blogpost.title).to.be.a('string');
+          expect(blogpost.content).to.be.a('string');
+          expect(blogpost.author).to.be.a('string');
+          expect(blogpost.publishDate).to.be.a('number');
+        });
+      });
+    });
+  
+  
+  
+  
+  
+  
   });
