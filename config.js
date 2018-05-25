ee7cfc868528300edacc3bd01bacfa8cb8db89bf
Kenny Lam <kmlam@ucdavis.edu>
2018-05-24 18:08:08 -0700

    using heroku

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogPostDb';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://kmlam:testing123@ds135540.mlab.com:35540/blogpost-app';
 exports.PORT = process.env.PORT || 8080;
